4c74bc791d526c28cc30567acfea946fac7f2640
trandaitai <trandaitai2005@gmail.com>
2025-12-12 00:43:23 +0700

    thay doi router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,70 +1,13 @@
 import { Fragment } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DefaultLayout from "./components/layout/DefaultLayout";
-import { protectedRoutes, authRoutes ,publicRoutes} from "./Routes";
+import {  router} from "./Routes/AppRoutes";
+import { RouterProvider } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* Public Routes */}
-        {publicRoutes.map((route, index) => {
-          const Page = route.component;
-          const Layout =
-            route.layout === null ? Fragment : route.layout ?? DefaultLayout;
-
-          return (
-            <Route
-              key={index}
-              path={route.path}
-              element={
-                <Layout>
-                  <Page />
-                </Layout>
-              }
-            />
-          );
-        })}
-
-        {/* Protected Routes */}
-        {protectedRoutes.map((route, index) => {
-          const Page = route.component;
-          const Layout =
-            route.layout === null ? Fragment : route.layout ?? DefaultLayout;
-
-          return (
-            <Route
-              key={index}
-              path={route.path}
-              element={
-                <Layout>
-                  <Page />
-                </Layout>
-              }
-            />
-          );
-        })}
-
-        {/* Auth Routes */}
-        {authRoutes.map((route, index) => {
-          const Page = route.component;
-          const Layout =
-            route.layout === null ? Fragment : route.layout ?? DefaultLayout;
-
-          return (
-            <Route
-              key={index}
-              path={route.path}
-              element={
-                <Layout>
-                  <Page />
-                </Layout>
-              }
-            />
-          );
-        })}
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router}
+    />
   );
 }
 
